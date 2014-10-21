53b6c102d4497099db7bcba673110aeff98470f3
Thomas Skardal <thomas.skardal@gmail.com>
2014-10-21 10:00:43 +0200

    Revealing recursively

--- src/main/scala/Minesweeper.scala
+++ src/main/scala/Minesweeper.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import scala.util.Random
 
 trait Cell
@@ -7,19 +8,69 @@ case class Unknown(covers: Cell) extends Cell
 case class Flag(covers: Cell) extends Cell
 case class Unsafe(surrounding: Int) extends Cell
 
+case class Point(x: Int, y: Int)
+
 /** Minesweeper
   * 
   * Brett som består av celler
   * Celle angis av x- og y-koordinat
   * Hvis en celle er safe må vi ekspandere rekursivt til vi treffer en unsafe
+  * 
+  * mines: Set[(Int, Int)], board: Map[(Int, Int), Cell]
   */
+class Minesweeper(width: Int, height: Int) {
+  private var theBoard = emptyBoard
+  def board = theBoard
+  
+  private def emptyBoard: Map[Point, Cell] = {
+    val tmp = for {
+      x <- 0 until width
+      y <- 0 until height
+    } yield Point(x, y) -> Unknown(Safe)
+    Map(tmp: _*)
+  }
+  
+  def reveal(p: Point) {
+    theBoard = reveal(Set(p), Set.empty, board)
+  }
+  
+  @tailrec
+  private def reveal(ps: Set[Point], revealed: Set[Point], board: Map[Point, Cell]): Map[Point, Cell] = {
+    val remaining = ps.diff(revealed)
+    if (remaining.isEmpty) board
+    else {
+      val current = remaining.head
+      val neighs = findNeighbours(current, board)
+      val surroundedBy = neighs.map(board(_)).map {
+        case Unknown(o) => o
+        case Flag(o) => o
+        case c:Cell => c   
+      }.count {
+        case Mine => true
+        case _ => false
+      }
 
-class Minesweeper(mines: Set[(Int, Int)], board: Map[(Int, Int), Cell]) {
-  val hoist: List[List[Cell]] = Nil
-  hoist.updated(2, hoist(2).updated(1, Safe))
+      val _ps = remaining ++ neighs
+      val _revealed = revealed + current 
+      val newCell = if (surroundedBy > 0) Unsafe(surroundedBy) else Safe
+      val _board = board + (current -> newCell)
+    
+      reveal(_ps, _revealed, _board)
+    }      
+  }
+
+  private def findNeighbours(p: Point, board: Map[Point, Cell]) = for {
+    dx <- -1 to 1
+    dy <- -1 to 1
+    nx = p.x + dx
+    ny = p.y + dy
+    if nx >= 0 && nx < width
+    if ny >= 0 && ny < height
+  } yield Point(nx, ny)
 }
 
 object Minesweeper {
+  
   def generate(numberOfMines: Int, width: Int, height: Int): Minesweeper = {    
     val mines =  Stream.continually(Mine).take(numberOfMines)
     val random = Random.shuffle(mines)
--- src/test/scala/MinesweeperTest.scala
+++ src/test/scala/MinesweeperTest.scala
@@ -1,7 +1,41 @@
 import org.scalatest.FunSuite
 
 class MinesweeperTest extends FunSuite {
-  test("random mines") {
+  test("creating a new game with a given dimension") {
+    val game = new Minesweeper(5, 5)
+    val allCells = game.board.values
+    assert(allCells.size === 5 * 5)
+  }
+
+  test("a fresh game contains only unknown cells") {
+    val game = new Minesweeper(5, 5)
+    val allCells = game.board.values
+    assert(allCells.forall({
+      case u: Unknown => true
+      case _ => false
+    }))
+  }
+  
+  test("revealing an unknown cell gives a board with fewer unknown cells") {
+    val game = new Minesweeper(5, 5)    
+    val before = game.board.values.count({
+      case u: Unknown => true
+      case _ => false
+    })
+    game.reveal(Point(0, 0))
+    val after = game.board.values.count {
+      case u: Unknown => true
+      case _ => false
+    }
     
+    assert(after < before)
+  }
+  
+  test("revealing a safe cell also reveal its neighbours") {
+    val game = new Minesweeper(5, 5)
+    game.reveal(Point(0, 0))
+    val safeCells = game.board.values.count(_ == Safe)
+    assert(safeCells === 5 * 5)
   }
 }
+  
